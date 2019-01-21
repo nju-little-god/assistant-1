3041f340b5cee594fbdc749427aedb1dd34113df
WilliamLeaves <Williamtheleaves@gmail.com>
2019-01-21 18:02:23 +0800

    1.21：实现登录功能
    前端的登录已经实现
    超时重新登录功能和注册功能还没有实现，交给你们了

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 //app.js
 App({
+  baseUrl: 'http://localhost:8080',
   onLaunch: function () {
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
+    this.login()
   },
   getUserInfo: function (cb) {
     var that = this
@@ -12,20 +14,45 @@ App({
       typeof cb == "function" && cb(this.globalData.userInfo)
     } else {
       //调用登录接口
-      wx.login({
-        success: function () {
-          wx.getUserInfo({
-            success: function (res) {
-              that.globalData.userInfo = res.userInfo
-              typeof cb == "function" && cb(that.globalData.userInfo)
-            }
-          })
-        }
-      })
+      login()
     }
   },
+  login:function(){
+    var that=this
+    wx.login({
+      success(res) {
+        wx.request({
+          url:that.baseUrl + '/',
+          method: "POST",
+          header: {
+            "Content-Type": "application/x-www-form-urlencoded"
+          },
+          data: {
+            userCode: res.code
+          },
+          success(res) {
+            if(res.result==1){
+              that.globalData.sessionKey = res.data.sessionKey
+            }
+            else if(res.result==2){
+              //实现注册界面
+            
+            }else if(res.result==-1){
+              console.log(res.message)
+            }
+          }
+        })
+        wx.getUserInfo({
+          success: function (res) {
+            that.globalData.userInfo = res.userInfo
+            typeof cb == "function" && cb(that.globalData.userInfo)
+          }
+        })
+      }
+    })
+  },
   globalData: {
-    userInfo: null
+    userInfo: null,
+    sessionKey: ""
   }
-
 })
